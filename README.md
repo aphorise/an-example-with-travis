040ca13c6f9212f344844ee2ec1845493798a0a5
Mehdi Ahmadi <aphorise@gmail.com>
2020-01-16 11:02:03 +0100

    Test case added, travis & README iupdated accordingly.

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 A simple script with travis-ci
 
 TODO:
-[ ] - Add test case for hello-world & ensure its working with Travis.i
 
 DONE:
 [x] - Add hello-world.sh script
 [x] - Add travis-ci
+[x] - Add test case for hello-world & ensure its working with Travis.i
---
+++ test_hello-world.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+EXPECT="HELLO WORLD2" ;
+OUTPUT="$(bash hello-world.sh)" ;
+
+MSG_GOOD="GREAT output as expected:\n${OUTPUT}\n\nPASSED\n"
+MSG_BAD="ERROR unexpected output:\n${OUTPUT}\nWas expecting: ${EXPECTING}\n\nFAILED!\n"
+
+if [[ ${OUTPUT} == ${EXPECT} ]] ; then
+	printf "${MSG_GOOD}" ;
+else
+	printf "${MSG_BAD}" ;
+	exit 1 ;
+fi ;
